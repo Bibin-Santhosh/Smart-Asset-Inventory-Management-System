1db39889d3bd8657340b2f473fad2711e1fa3f94
Bibin V Santhosh <bibinvsanthosh@gmail.com>
2026-01-05 12:31:23 +0530

    Add Db Wait logic to start script

--- start.sh
+++ start.sh
@@ -1,6 +1,25 @@
 #!/usr/bin/env bash
 set -o errexit
 
+echo "Waiting for database to be ready..."
+
+python - << 'EOF'
+import time
+import psycopg2
+import os
+
+db_url = os.environ.get("DATABASE_URL")
+
+while True:
+    try:
+        psycopg2.connect(db_url)
+        print("Database is ready!")
+        break
+    except Exception as e:
+        print("Database not ready, waiting...")
+        time.sleep(3)
+EOF
+
 echo "Running migrations..."
 python manage.py migrate --noinput
 
