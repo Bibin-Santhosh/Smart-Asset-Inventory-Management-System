1cd920cde6f51596ff38762c42f9fbeae2c718ee
Bibin V Santhosh <bibinvsanthosh@gmail.com>
2026-01-05 12:52:35 +0530

    Remove DATABASE_URL debug log

--- asset_management/settings.py
+++ asset_management/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
-print("DATABASE_URL =", os.environ.get("DATABASE_URL"))
+
 from pathlib import Path
 import dj_database_url
 
